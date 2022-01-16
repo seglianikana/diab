689f0580b64cf48ca989c8a7f7e8d14493332e8a
Yurii Kuzin <ykuzin@izigroup.nyc>
2022-01-16 23:10:27 +0200

    make it better

--- runserver.js
+++ runserver.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const mongoose = require("mongoose");
-const server = require("./server");
+const app = require("./server");
 const SERVER_PORT = 8080
 const SERVER_HOST = "127.0.0.1"
 
@@ -17,13 +17,14 @@ mongoose.connect(MONGO_URL, {
     )
     .catch(err => console.info(err));
 
+const port_number = process.env.PORT || 8080;
 
-const instance = server.listen(Number(SERVER_PORT), SERVER_HOST, () => {
-    console.info("Available on:", `${SERVER_HOST}:${SERVER_PORT}`)
+const instance = app.listen(Number(port_number), SERVER_HOST, () => {
+    console.info("Available on:", `${SERVER_HOST}:${port_number}`)
 });
 
 instance.on("listening", () =>
-    console.info("Available on:", `${SERVER_HOST}:${SERVER_PORT}`)
+    console.info("Available on:", `${SERVER_HOST}:${port_number}`)
 );
 instance.on("error", error => console.error(error));
 
